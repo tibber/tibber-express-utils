7ab78677d84639397a57266330c6b96a7b06bf55
Kjell Inge Rotnes <kirotnes@gmail.com>
2018-07-26 15:38:27 +0200

    Returning 404 when GET requests returns undefined

--- src/index.js
+++ src/index.js
@@ -35,7 +35,7 @@ export function jsonRouting(expressRouter, contextFunc) {
   expressRouter.expressDelete = expressRouter.delete;
 
   contextFunc = contextFunc || (req => req.context);
-  expressRouter.get = (path, middleWareFunc) => expressRouter.expressGet(path, middlewareFunc(r => 200, middleWareFunc, contextFunc));
+  expressRouter.get = (path, middleWareFunc) => expressRouter.expressGet(path, middlewareFunc(r => r == undefined ? 404 : 200, middleWareFunc, contextFunc));
   expressRouter.post = (path, middleWareFunc) => expressRouter.expressPost(path, middlewareFunc(r => r ? 202 : 204, middleWareFunc, contextFunc));
   expressRouter.patch = (path, middleWareFunc) => expressRouter.expressPatch(path, middlewareFunc(r => r ? 200 : 204, middleWareFunc, contextFunc));
   expressRouter.put = (path, middleWareFunc) => expressRouter.expressPut(path, middlewareFunc(r => r ? 200 : 204, middleWareFunc, contextFunc));
