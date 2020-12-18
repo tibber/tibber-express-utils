ac7cf0c5001569b2ff39bd1cc0d7b3efc40fc970
Sebastian Nemeth <sebastian@cleric.sh>
2020-12-18 16:41:45 +0200

    chore: clean up fake feature

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,3 @@ export * from './jsonRouting';
 export * from './jsonMiddleware';
 export * as Errors from './errors';
 export * from './types';
-
-/**
- * Adding a fake feature to test a pre-release.
- */
