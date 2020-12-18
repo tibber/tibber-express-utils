e8714d97bd5752d9cff0a7fe93515572b4774d31
Sebastian Nemeth <sebastian@cleric.sh>
2020-12-18 15:19:43 +0200

    feat: add a fake feature for testing pre-releases

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,7 @@ export * from './jsonRouting';
 export * from './jsonMiddleware';
 export * as Errors from './errors';
 export * from './types';
+
+/**
+ * Adding a fake feature to test a pre-release.
+ */
