96ac162ba4c9978e83fba752be2354c3e265feff
Tobias Neil <98805486+tobias-at-tibber@users.noreply.github.com>
2023-03-06 10:29:10 +0100

    Added extensions to problem details (#32)
    * Added extensions to problem details
    
    * feat: made detail/instance optional (as per RFC)
    
    * chore: updated yarn lockfile, made 'husky install' part of prep script

--- src/errors/ProblemDetailsError.ts
+++ src/errors/ProblemDetailsError.ts
@@ -1,27 +1,34 @@
 import {HttpError} from './HttpError';
 
 export type ProblemDetailsArgs = {
-  detail: string;
-  instance: string;
+  detail?: string;
+  instance?: string;
   statusCode: number;
   title: string;
   type: string;
+  extensions?: {
+    [k: string]: unknown;
+  };
 };
 
 export class ProblemDetailsError extends HttpError {
-  public detail: string;
   public type: string;
-  public instance: string;
   public title: string;
+  public detail?: string;
+  public instance?: string;
+  public extensions?: {
+    [k: string]: unknown;
+  };
 
   constructor(args: ProblemDetailsArgs) {
-    const {detail, instance, statusCode, title, type} = args;
+    const {detail, instance, statusCode, title, type, extensions} = args;
 
-    super(detail, statusCode);
+    super(detail ?? title, statusCode);
 
     this.detail = detail;
     this.type = type;
     this.instance = instance;
     this.title = title;
+    this.extensions = extensions ?? {};
   }
 }
